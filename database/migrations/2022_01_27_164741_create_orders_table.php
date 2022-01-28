f8aa8c14b5d30c7d2811e5d865c2c34f9ccb365b
oah1006 <bnhao10062001@gmail.com>
2022-01-28 22:45:58 +0700

    db: update index users and orders table migration

--- database/migrations/2022_01_27_164741_create_orders_table.php
+++ database/migrations/2022_01_27_164741_create_orders_table.php
@@ -18,20 +18,18 @@ class CreateOrdersTable extends Migration
             $table->string('code')->unique();
             $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('promotion_id')->nullable();
-            $table->enum('status', ['pending', 'delivering', 'succeeded', 'canceled']);
-            $table->unsignedBigInteger('total');
+            $table->enum('status', ['pending', 'delivering', 'succeeded', 'canceled'])->index();
+            $table->unsignedBigInteger('total')->index();
             $table->unsignedBigInteger('sub_total');
             $table->unsignedBigInteger('shipping_fee');
             $table->unsignedBigInteger('discount');
-            $table->enum('payment_method', ['cod', 'banking']);
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->text('address');
-            $table->string('phone_number');
+            $table->enum('payment_method', ['cod', 'banking'])->index();
+            $table->string('first_name')->index();
+            $table->string('last_name')->index();
+            $table->text('address')->index();
+            $table->string('phone_number')->index();
             $table->timestamps();
             $table->softDeletes();
-
-            $table->index(['status', 'payment_method', 'total', 'first_name', 'last_name', 'address', 'phone_number']);
         });
     }
 
